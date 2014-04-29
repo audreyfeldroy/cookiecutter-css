6c2f152a64427d0ebcb1474c6db84b44f39f416b
Audrey Roy <audreyr@gmail.com>
2014-04-29 12:45:38 -0700

    Temporarily disable prep - depends on unimplemented feature in Cookiecutter.

--- prep.py
+++ prep.py
@@ -6,5 +6,5 @@ from cookiecutter.main import cookiecutter
 from cookiecutter.utils import work_in
 
 
-with work_in('{{cookiecutter.repo_name}}/'):
-    os.system("npm install")
+# with work_in('{{cookiecutter.repo_name}}/'):
+#     os.system("npm install")
